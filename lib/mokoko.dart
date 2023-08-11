2025732ab4826f28d223f35cb532c0252773bbac
hazle <aside.hazle@gmail.com>
2023-08-11 17:54:59 +0900

    fix: character null check

--- lib/mokoko.dart
+++ lib/mokoko.dart
@@ -8,7 +8,6 @@ const base = 'developer-lostark.game.onstove.com';
 class Mokoko {
   final String apiKey;
 
-
   Mokoko(this.apiKey);
 
   Future<List<Character>?> characters(String characterName) async {
@@ -16,16 +15,15 @@ class Mokoko {
     final path = '/characters/$characterName/siblings';
     final url = Uri.https(base, path);
 
-    final response = await http.get(
-      Uri.https(base, path),
-      headers: {
-        "accept": "application/json",
-        "authorization": "bearer $apiKey",
-      }
-    );
+    final response = await http.get(Uri.https(base, path), headers: {
+      "accept": "application/json",
+      "authorization": "bearer $apiKey",
+    });
 
-    if(response.statusCode == 200) {
-      print(response.body);
+    if (response.statusCode == 200) {
+      if (response.body.isEmpty || response.body == '[]') {
+        return null;
+      }
       final json = jsonDecode(response.body) as List; // Parse string as JSON
       final characters = json.map((e) => Character.fromJson(e)).toList();
 
